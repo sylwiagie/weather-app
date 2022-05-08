423854175bb0f0762e462aca9ded4263050b5b26
sylwiagie <guzdkowa@gmail.com>
2022-05-08 22:40:46 +0200

    Add CSS to weather app

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+import CurrentLocation from "./CurrentLocation.js";
+const currentLoc = new CurrentLocation();
+
+const initApp = () => {
+    //add listeneres
+
+    // set up
+
+    // load weather 
+}
+
+document.addEventListener("DOMContentLoaded", initApp);
